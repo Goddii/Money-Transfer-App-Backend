1caba6f5d5c71bcccb342f0b40afac58815866ea
Goddii <otienogodwin23@gmail.com>
2026-08-20 12:43:53 +0300

    feat: initialize flask app, extensions, register models and blueprints

---
+++ app/__init__.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from app.config import Config
+from app.extensions import db, migrate, jwt, cors
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object(Config)
+
+    # Initialize extensions
+    db.init_app(app)
+    migrate.init_app(app, db)
+    jwt.init_app(app)
+    cors.init_app(app, resources={r"/api/*": {"origins": app.config['CORS_ORIGINS'].split(',')}})
+
+
+    #import models to register them with SQLAlchemy
+    from app.models.user import User
+    from app.models.beneficiary import Beneficiary
+
+    
+    # Register blueprints
+    from app.routes import auth_bp, user_bp
+    app.register_blueprint(auth_bp)
+    app.register_blueprint(user_bp)
+    
+
+    return app
