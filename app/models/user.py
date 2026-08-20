05a5141d05fccaff4fd5609a9163c29c96089de3
kirukufaith5-collab <kirukufaith5@gmail.com>
2026-08-20 19:13:40 +0300

    Feat-Create Wallet class

--- app/models/user.py
+++ app/models/user.py
@@ -26,3 +26,12 @@ class User(db.Model):
 
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
+
+class Wallet(db.Model):
+    __tablename__ = 'wallets'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    balance = db.Column(db.Numeric(12, 2), default=0.00)
+    currency = db.Column(db.String(3), default='USD')
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
