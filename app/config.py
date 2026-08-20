6b771c65fe555b031175846d1855c25c76d9c8fb
Goddii <otienogodwin23@gmail.com>
2026-08-20 12:35:20 +0300

    chore: add cors config

--- app/config.py
+++ app/config.py
@@ -6,7 +6,7 @@ class Config:
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'dev-jwt-secret-key-change-me')
 
-    
+    CORS_ORIGINS = os.environ.get('CORS_ORIGINS', 'http://localhost:5173')  
 
 
 
