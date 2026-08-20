4e6e24bcf0fbd52c16a5f2a9603c0fd7257c03de
Goddii <otienogodwin23@gmail.com>
2026-08-20 13:08:45 +0300

    fix: remove unique

--- app/models/user.py
+++ app/models/user.py
@@ -7,8 +7,8 @@ class User(db.Model):
     __tablename__ = 'users'
 
     id = db.Column(db.Integer, primary_key=True)
-    first_name = db.Column(db.String(100), unique=True, nullable=False)
-    last_name = db.Column(db.String(100), unique=True, nullable=False)
+    first_name = db.Column(db.String(100), nullable=False)
+    last_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(255), unique=True, nullable=False)
     phone_number = db.Column(db.String(20), unique=True, nullable=False)
     password_hash = db.Column(db.String(128), nullable=False)
