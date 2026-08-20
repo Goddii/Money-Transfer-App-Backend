cc5b589ca674bb3ebc62785782c0826d75fa9f51
Goddii <otienogodwin23@gmail.com>
2026-08-20 14:06:54 +0300

    feat(routes): add update user api endpoint

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -34,3 +34,47 @@ def get_current_user():
             }
         }
     ), 200
+
+@user_bp.put("/me")
+@jwt_required_custom
+def update_current_user():
+
+    user_id = get_jwt_identity()
+
+    user = UserService.get_user_by_id(int(user_id))
+
+    if not user:
+        return jsonify(
+            {
+                "success": False,
+                "message": "User not found.",
+            }
+        ), 404
+
+    try:
+        data = request.get_json(silent=True) or {}
+
+        validated_data = validate_user_update(data)
+
+        user = UserService.update_user(
+            user=user,
+            **validated_data,
+        )
+
+        return jsonify(
+            {
+                "success": True,
+                "message": "Profile updated successfully.",
+                "data": {
+                    "user": user.to_dict(),
+                },
+            }
+        ), 200
+
+    except ValueError as error:
+        return jsonify(
+            {
+                "success": False,
+                "message": str(error),
+            }
+        ), 400
